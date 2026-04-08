36362be7c2fbcf0571d1932c49c0d87eef09158e
Liafanx <liafanx@gmail.com>
2026-04-08 17:52:40 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -uo pipefail
 
-REPO_URL="https://raw.githubusercontent.com/Liafanx/AZ-WARP/main"
+REPO_URL="https://raw.githubusercontent.com/Liafanx/AZ-WARP/1.1.0pre"
 SB_VERSION="1.13.5"
 
 RED='\033[0;31m'
